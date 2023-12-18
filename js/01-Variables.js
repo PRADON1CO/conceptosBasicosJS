3060ab988e51014832244824acc824fd1e251e23
Prado Nicolas <nicolas88prado@gmail.com>
2023-12-18 20:28:52 -0300

    Agregamos mensajes por consola, documento html y ventana emergente

---
+++ js/01-Variables.js
@@ -0,0 +1,18 @@
+// comentario de unica linea
+
+/*
+Esto es un comentario
+de multiples lineas
+jejej
+
+*/
+
+// Mostrar comentarios desde le archivo de js
+console.log('Mensaje por consola')
+console.log("Mensaje por consola")
+
+// Mostrar un comentario en el documento html
+document.write('<p class="text-center text-primary">Primer mensaje en el documento html</p>')
+
+// Ventana emergentes
+alert('Ejemplo de un alerta')
