804c7243af33e9f6ea2ec734026ea8970ee78230
Prado Nicolas <nicolas88prado@gmail.com>
2023-12-18 21:18:15 -0300

    creamos variables con let y const

--- js/01-Variables.js
+++ js/01-Variables.js
@@ -15,4 +15,24 @@ console.log("Mensaje por consola")
 document.write('<p class="text-center text-primary">Primer mensaje en el documento html</p>')
 
 // Ventana emergentes
-alert('Ejemplo de un alerta')
+//alert('Ejemplo de un alerta')
+
+// Declarar e inacializar una variable
+//  declarar o crear una variale (var - let - const)
+
+let regaloNavidad = 'Iphone 15 Pro Max';
+
+document.write('Regalo de navidad 2023: '+ regaloNavidad+ '<br>');
+
+// modificar el contenido de una variable creada con la palabra let
+regaloNavidad = 'Rtx4090 Rog Strix';
+
+document.write('Regalo de navidad 2023: '+ regaloNavidad);
+
+const urlUniverse = 'https://universe.rollingcodeschool.com/my-courses';
+
+document.write('<br>Página de Universe '+ urlUniverse);
+
+//urlUniverse = 123;
+//document.write('<br>Página de Universe '+ urlUniverse);
+
