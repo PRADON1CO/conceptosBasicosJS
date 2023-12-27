49b534be895a1e0b5701daa6d29564f5e26772e1
Prado Nicolas <nicolas88prado@gmail.com>
2023-12-27 18:18:20 -0300

    agregamos un ejemplo de bucle for

--- js/bucles.js
+++ js/bucles.js
@@ -22,11 +22,20 @@ do{
     agregar un codigo para que la condicion se deje de cumplir
 */
 
-let renglon = 1;
+// let renglon = 1;
 
-do{
-    document.write(`<p>Renglon numero ${renglon} </p>`);
-    renglon ++;
-}while(renglon <=50);
+// do{
+//     document.write(`<p>Renglon numero ${renglon} </p>`);
+//     renglon ++;
+// }while(renglon <=50);
 
 //for
+/*
+    for(iniciar una variable; condicion logica; incrementar o decrementar una variable){
+        todas las lineas de codigo que quiero repetir varias veces
+    }
+*/
+
+for(let renglon = 1; renglon <= 50; renglon ++){
+    document.write(`<p>Renglon numero ${renglon} </p>`);
+}
