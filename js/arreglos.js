2e5ac8250ca83b416b019b244867714a02565728
Prado Nicolas <nicolas88prado@gmail.com>
2024-02-05 20:41:00 -0300

    agregamos elementos de distintas maneras al array

--- js/arreglos.js
+++ js/arreglos.js
@@ -17,11 +17,46 @@ document.write(`<p>El Juego numero 1 es ${juegos[posicion]}</p>`);
 document.write(`<p>El Juego numero 20 es ${juegos[20]}</p>`);
 
 
-document.write('<h2>Array de juegos 🎮</h2>');
+document.write(`<h2>Array de juegos 🎮 (${juegos.length})</h2>`);
 
 document.write(`<ul>`)
 for (let posicion = 0; posicion < juegos.length ;posicion++){
-    document.write(`<li> ${juegos[posicion]} </li>`)
+    document.write(`<li> ${juegos[posicion]} </li>`);
+}
+
+document.write(`</ul>`);
+
+//Agragar elementos al array
+juegos.unshift('Dark sould', 'dota');
+
+document.write(`<h2>Nuevo juego al rpincipio del array 🎮 (${juegos.length})</h2>`);
+
+document.write(`<ul>`);
+for (let posicion = 0; posicion < juegos.length ;posicion++){
+    document.write(`<li> ${juegos[posicion]} </li>`);
 }
 
 document.write(`</ul>`)
+
+juegos.push('MARIO BROS');
+
+document.write(`<h2>Nuevo juego al final del array 🎮 (${juegos.length})</h2>`);
+
+document.write(`<ul>`);
+for (let posicion = 0; posicion < juegos.length ;posicion++){
+    document.write(`<li> ${juegos[posicion]} </li>`);
+}
+
+document.write(`</ul>`);
+
+juegos.splice(6 , 0, 'terraria');
+
+document.write(`<h2>Nuevo juego en la posicio 6 del juego 🎮 (${juegos.length})</h2>`);
+
+document.write(`<ul>`)
+for (let posicion = 0; posicion < juegos.length ;posicion++){
+    document.write(`<li> ${juegos[posicion]} </li>`);
+}
+
+document.write(`</ul>`);
+
