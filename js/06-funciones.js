00d19648b18084b847c382e4beb53b46c50ac80b
Prado Nicolas <nicolas88prado@gmail.com>
2024-02-07 21:12:31 -0300

    Agregamos un ejemplo e funciones con parametros y sin parametros

---
+++ js/06-funciones.js
@@ -0,0 +1,45 @@
+//funciones declarativas sin parametros
+function top3Juegos(){
+//aqui agrego la logica de la funcion
+    document.write(`<h2>Top 3 de juegos </h2>`);
+    document.write(`<ul>
+    <li>Resident evil Village</li>
+    <li>Red dead redemotion 2</li>
+    <li>The binding of Isaac</li>
+    </ul>`)
+}
+
+//Funciones con parametros
+function saludarJugador(nombre, juego){
+    document.write(`<p>Hola ${nombre}, biencenido al juego "${juego}". Que tengas una exelente jornada de juegos.</p>`);
+
+}
+
+//Funciones que retornen un valor
+function juegoClasico(nombreJuego, anioLanzamiento){
+    const anioActual = new Date().getFullYear();
+    const antiguedad =  anioActual - anioLanzamiento;
+    let respuesta = '';
+    if(antiguedad >= 10){
+        respuesta = `El juego ${nombreJuego} es considerado un clasico, tiene una antiguedad de ${antiguedad} años`;
+    }else{
+        respuesta = `El juego ${nombreJuego} no es un clasico, tiene una antiguedad de ${antiguedad} años`;
+
+    }
+    return respuesta;
+}
+
+//Llamar o invocar
+top3Juegos();
+saludarJugador('Lucas', 'Minecraft');
+
+const nombreJugador = prompt('Ingrese el  nombre del jugador: ');
+const juego = prompt('Ingrese su nombre: ');
+
+saludarJugador(nombreJugador, juego);
+saludarJugador(nombreJugador);
+
+const esClasico = juegoClasico('GTA SA', 2004);
+document.write(esClasico)
+
+document.write(`<p>${juegoClasico('contra', 1987)}</p>`)
