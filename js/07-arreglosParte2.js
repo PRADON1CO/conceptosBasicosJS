c1c91c43da23ed49ba1c3fd40ada714c6b685536
Prado Nicolas <nicolas88prado@gmail.com>
2024-02-19 20:39:55 -0300

    agregamos nuevas funciones a los arreglos

---
+++ js/07-arreglosParte2.js
@@ -0,0 +1,59 @@
+const perifericos = [
+    "Ratón Logitech MX Master 3",
+    "Teclado mecánico Corsair K95 RGB Platinum",
+    "Monitor LG UltraGear 27GN950-B 4K",
+    "Auriculares Bose QuietComfort 45",
+    "Silla gaming Secretlab Omega",
+    "Webcam Logitech C920 Pro HD",
+    "Micrófono Blue Yeti USB",
+    "Soporte para portátil ajustable Nexstand",
+    "Lámpara de escritorio LED BenQ e-Reading",
+    "Base de carga inalámbrica Anker PowerWave",
+    "Smartwatch Garmin Forerunner 945",
+    "Auriculares inalámbricos Sony WH-1000XM4",
+    "Monitor Dell Ultrasharp U2720Q 27 Pulgadas 4K",
+    "Silla ergonómica Steelcase Gesture",
+    "Teclado inalámbrico Logitech MX Keys",
+    "Webcam 4K Logitech Brio",
+    "Auriculares gaming Razer BlackShark V2",
+    "Soporte para monitor de madera",
+    "Auriculares deportivos Powerbeats Pro",
+    "Teclado mecánico Razer Huntsman Elite",
+  ];
+
+  const mostrarArray = (titulo, arrayNuevo) => {
+    document.write(`<h2>${titulo}:</h2>`);
+    document.write(`<ul>`)
+    //metodo inmutable map
+    arrayNuevo.map((producto) => document.write(`<li>${producto}</li>`) )
+    document.write(`</ul>`);
+}
+
+//invocar a la funcion mostrarArray
+mostrarArray('Lista de productos', perifericos);
+
+//verificar si el periferico: Auriculares gaming Razer BlackShark V2 existe en el array
+//ejemplos con incledes
+const productoParaBuscar = `Auriculares gaming Razer BlackShark`;
+document.write(`<p>Existe el producto: ${productoParaBuscar}??</p>`);
+
+// if(perifericos.includes(productoParaBuscar)){
+//     document.write(`<p>El producto: ${productoParaBuscar}, SI Existe.</p>`);
+// }else{
+//     document.write(`<p>El producto: ${productoParaBuscar}, no Existe.</p>`);
+// }
+
+//operador ternario => (condicion logica)? agrego lo que quiero si se cumple la condicion : agrego lo que quiero si no se cumple la condicion ;
+
+(perifericos.includes(productoParaBuscar))? document.write(`<p>El producto: ${productoParaBuscar}, SI Existe.</p>`) : document.write(`<p>El producto: ${productoParaBuscar}, no Existe.</p>`);
+
+//crear un filtro de elementos
+const productosFiltrados = perifericos.filter((producto) => producto.includes(`Auriculares`))
+mostrarArray('Auriculares disponibles:', productosFiltrados);
+
+//buscar un elemto, Webcam 4K Logitech Brio
+
+//const productoBuscado = perifericos.find((producto)=> producto === 'Webcam 4K Logitech Brio'); 
+const productoBuscado = perifericos.find((producto) => producto.includes('Webcam 4K'));
+document.write(`<p>El producto: ${productoBuscado}.</p>`);
+
